8692e97fcb495e17ee5287c7d91c7cec80753ca4
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-02 14:07:39 -0300

    header done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,19 @@
-import {Fire, Scroll, Timer} from 'phosphor-react';
-import {HeaderContainer} from './styles';
+import { Fire, Scroll, Timer } from 'phosphor-react';
+import { HeaderContainer } from './styles';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
-	return (
-		<HeaderContainer>
-			<Fire size={40}/>
-			<nav>
-				<a href='/'>
-					<Timer size={28}/>
-				</a>
-				<a href='/history'>
-					<Scroll size={28}/>
-				</a>
-			</nav>
-		</HeaderContainer>
-	);
+  return (
+    <HeaderContainer>
+      <Fire size={40} />
+      <nav>
+        <NavLink to='/'>
+          <Timer size={28} />
+        </NavLink>
+        <NavLink to='/history'>
+          <Scroll size={28} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
 }
