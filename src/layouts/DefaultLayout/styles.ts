6d020c9b6eae542368692dac4305bc9ed1f126e9
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-02 13:41:29 -0300

    wip

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const LayoutStyle = styled.div`
+export const LayoutContainer = styled.div`
   max-width: 74rem;
   height: calc(100vh - 10rem);
   margin: 5rem auto;
