2e41bf53b41dfdcaad2c1c5f641645873e9f220f
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-12 18:15:51 -0300

    wip

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export default function History() {
   return (
@@ -22,19 +22,19 @@ export default function History() {
               <td>Task name</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Finished</td>
+              <td><Status kind="done">Finished</Status></td>
             </tr>
             <tr>
               <td>Task name</td>
               <td>30 minutes</td>
               <td>4 months ago</td>
-              <td>Paused</td>
+              <td><Status kind="paused">Paused</Status></td>
             </tr>
             <tr>
               <td>Task name</td>
               <td>15 minutes</td>
               <td>7 months ago</td>
-              <td>Stopped</td>
+              <td><Status kind="stopped">Stopped</Status></td>
             </tr>
           </tbody>
         </table>
