85a7a4923dcce3863bf14248b4c7528ca27eb116
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-14 11:26:43 -0300

    move context to a isolated component

---
+++ src/contexts/CycleContext.tsx
@@ -0,0 +1,94 @@
+import { ReactNode, createContext, useState } from "react"
+
+interface CycleData {
+  task: string
+  timeInMin: number
+}
+
+export interface Cycle {
+  id: string
+  task: string
+  timeInMin: number,
+  date: Date,
+  interruptedDate?: Date,
+  finishedDate?: Date
+}
+
+interface CycleContextType {
+  cycles: Cycle[]
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  secondsPassed: number
+  finishCurrentCycle: () => void
+  handleInterruptCycle: () => void
+  createNewCycle: (c: CycleData) => void
+  setSecondsPassedProxy: (n: number) => void
+}
+
+export const CycleContext = createContext({} as CycleContextType)
+
+export function CycleContextProvider({ children }: { children: ReactNode }) {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [secondsPassed, setSecondsPassed] = useState(0);
+
+  const activeCycle = cycles.find(c => c.id === activeCycleId);
+
+  function createNewCycle(data: CycleData) {
+    const id = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      timeInMin: data.timeInMin,
+      date: new Date()
+    };
+
+    setCycles(state => [...state, newCycle]);
+    setActiveCycleId(id);
+    setSecondsPassed(0);
+
+    reset();
+  }
+
+  function finishCurrentCycle() {
+    setCycles(state => state.map(state => {
+      console.log(state)
+      return state.id === activeCycleId
+        ? { ...state, finishedDate: new Date() }
+        : state;
+    }))
+
+    setActiveCycleId(null);
+  }
+
+  function handleInterruptCycle() {
+    setCycles(
+      cycles.map(state => {
+        if (state.id === activeCycleId) return { ...state, interruptedDate: new Date() };
+        return state;
+      })
+    )
+
+    setActiveCycleId(null);
+  }
+
+  function setSecondsPassedProxy(n: number) {
+    setSecondsPassed(n)
+  }
+
+  return (
+    <CycleContext.Provider value={{
+      cycles,
+      activeCycle,
+      activeCycleId,
+      finishCurrentCycle,
+      secondsPassed,
+      setSecondsPassedProxy,
+      handleInterruptCycle,
+      createNewCycle
+    }}>
+      {children}
+    </CycleContext.Provider>
+  )
+}
