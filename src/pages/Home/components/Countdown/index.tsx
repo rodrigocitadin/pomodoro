91dc833cc7a4f27ae13795fc85a989c17f14e8ec
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-13 22:28:57 -0300

    countdown component finished (refact)

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,6 +1,42 @@
+import { useContext, useEffect, useState } from "react";
 import { CountdownContainer, Separator } from "./styles";
+import { CyclesContext } from "../..";
+import { differenceInSeconds } from "date-fns";
 
 export default function Countdown() {
+  const { activeCycle, finishCurrentCycle } = useContext(CyclesContext);
+  const [secondsPassed, setSecondsPassed] = useState(0);
+
+  const timeInSec = activeCycle ? activeCycle.timeInMin * 60 : 0;
+  const currentTimeInSec = activeCycle ? timeInSec - secondsPassed : 0;
+
+  const minutes = Math.floor(currentTimeInSec / 60);
+  const seconds = currentTimeInSec % 60;
+
+  const minutesDisplay = String(minutes).padStart(2, '0');
+  const secondsDisplay = String(seconds).padStart(2, '0');
+
+  useEffect(() => {
+    let interval: number;
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        setSecondsPassed(differenceInSeconds(new Date(), activeCycle.date));
+
+        if (currentTimeInSec <= 0) {
+          finishCurrentCycle();
+          clearInterval(interval);
+        }
+      }, 1000)
+    }
+
+    return () => clearInterval(interval);
+  }, [activeCycle, currentTimeInSec])
+
+  useEffect(() => {
+    if (activeCycle) document.title = `${minutes}:${secondsDisplay} - Pomotimer`
+  }, [minutes, secondsDisplay, activeCycle])
+
   return (
     <CountdownContainer>
       <span>{minutesDisplay[0]}</span>
