b1471f2c4c23a1a5038f1f74eec77e6c161878e1
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-13 22:47:36 -0300

    finished refactoring cycle form

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,29 +1,33 @@
+import { useFormContext } from "react-hook-form";
 import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
+import { useContext } from "react";
+import { CyclesContext } from "../..";
 
 export default function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
+
   return (
-    <form onSubmit={handleSubmit(handleNewCycle)}>
-      <FormContainer>
-        <label htmlFor="task">I will work in</label>
-        <TaskInput
-          id="task"
-          placeholder="Task name"
-          disabled={!!activeCycle}
-          {...register('task')}
-        />
-        <label htmlFor="minutes">during</label>
-        <MinutesAmountInput
-          type="number"
-          id="time"
-          placeholder="0"
-          step={5}
-          min={1}
-          max={60}
-          disabled={!!activeCycle}
-          {...register('timeInMin', { valueAsNumber: true })}
-        />
-        <span>minutes</span>
-      </FormContainer>
-    </form>
+    <FormContainer>
+      <label htmlFor="task">I will work in</label>
+      <TaskInput
+        id="task"
+        placeholder="Task name"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+      <label htmlFor="minutes">during</label>
+      <MinutesAmountInput
+        type="number"
+        id="time"
+        placeholder="0"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('timeInMin', { valueAsNumber: true })}
+      />
+      <span>minutes</span>
+    </FormContainer>
   )
 }
